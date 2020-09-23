eb1eac6ea492ccf44761c0efb37e50e8eb01b3e0
antography <antography@anto.gg>
2020-09-22 18:08:02 -0700

    nmap complete

--- sockets/nmap.py
+++ sockets/nmap.py
@@ -9,11 +9,21 @@ import shlex
 from wm1 import socketio, app
 
 @socketio.on("nmapcall", namespace="/nmap")
-def nmapcall():
+def nmapcall(data):
+ 
   if app.config["nmap_child"]:
     socketio.send("map already running", namespace="/nmap")
     return
-  with subprocess.Popen(['nmap', '-v9',  '--stats-every', '10s', '-sV','localhost'],
+  host = data['host']
+  args = data['args'].split(' ')
+
+  if not host:
+    socketio.send("no host set", namespace="/nmap")
+    return
+  final = ['nmap'] + args + [host]
+
+  print(final)
+  with subprocess.Popen(final,
                           stdout=subprocess.PIPE,
                           stdin=subprocess.PIPE,
                           stderr=subprocess.PIPE,
@@ -23,5 +33,4 @@ def nmapcall():
     print(app.config['nmap_child'])
     for line in process.stdout:
         line = line.rstrip()
-        print(f"line = {line}")
         socketio.emit("nmapout", {"output": line}, namespace="/nmap")
--- static/scripts/nmap.js
+++ static/scripts/nmap.js
@@ -1,10 +1,12 @@
 var socket = io('/nmap');
 
+var helploaded = false
 // prevent nmap from doing funky stuff and sending the controls offscreen
 window.onload = function () {
   let nmbar = document.getElementById('nmapbar');
   let nmman = document.getElementById('frameableframe');
   nmman.style.width = ("calc(100vw - " + nmbar.clientWidth + 'px)');
+  nmman.style.maxWidth = ("calc(100vw - " + nmbar.clientWidth + 'px)');
 }
 
 var settingsMirror = CodeMirror(document.getElementById("framingframeday1"), {
@@ -16,8 +18,29 @@ var settingsMirror = CodeMirror(document.getElementById("framingframeday1"), {
 });
 settingsMirror.setSize(null, '100%');
 
+function togglehelp(){
+  console.log("help requested L:D")
+  var element = document.getElementById("helpmodal");
+  element.classList.toggle("is-active");
+  if (!helploaded) {
+    fetch('/static/help/nmap.html')
+      .then(response => response.text())
+      .then((data) => {
+        document.getElementById("helptext").innerHTML= data
+      })
+    helploaded = true
+  }
+}
+
 function callnmap() {
-  socket.emit('nmapcall')
+
+  var options = {
+    host: document.getElementById("nmhost").value,
+    args: document.getElementById("nmoptions").value
+  }
+  console.log(options)
+  socket.emit('nmapcall', options)
+
 }
 
 socket.on("message", function(data){
@@ -26,6 +49,6 @@ socket.on("message", function(data){
 
 socket.on("nmapout", function(data){
   var doc = settingsMirror.getDoc();
-  console.log(data)
   doc.replaceRange(data.output+ "\n", {line: Infinity})
 })
+
